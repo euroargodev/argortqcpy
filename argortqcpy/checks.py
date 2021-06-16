bdf03837aa89890a536c307a4245c0e3f9a864aa
Andrew Hale <andhal@bodc.ac.uk>
2021-06-16 09:21:10 +0100

    Add check base class

--- argortqcpy/checks.py
+++ argortqcpy/checks.py
@@ -1,1 +1,35 @@
 """Implement Argo checks."""
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from netCDF4 import Dataset
+
+
+class CheckBase(ABC):
+    """Abstract base class for Argo checks."""
+
+    argo_test_id: int
+    argo_test_name: str
+
+    def __init__(self, profile: Dataset, profile_previous: Optional[Dataset]) -> None:
+        """Initialise the test with the relevant profile and its precursor.
+
+        Args:
+            profile: The profile of interest to be checked.
+            profile_previous: The profile prior to the profile of interest.
+                ``None`` if the profile of interest is the first.
+        """
+        self._profile = profile
+        self._profile_previous = profile_previous
+
+    @abstractmethod
+    def run(self) -> Dataset:
+        """Run the check.
+
+        Return: a Dataset of checked columns with given flags
+        """
+
+    def is_required(self) -> bool:  # pylint: disable=no-self-use
+        """Is the check required to run or not."""
+        return True

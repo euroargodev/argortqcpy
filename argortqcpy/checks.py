1feda27fbd0f8b1ccd23afd3f7e1f9bea61efeb1
Andrew Hale <andhal@bodc.ac.uk>
2021-06-16 16:31:22 +0100

    Add check output class

--- argortqcpy/checks.py
+++ argortqcpy/checks.py
@@ -1,11 +1,92 @@
 """Implement Argo checks."""
 
 from abc import ABC, abstractmethod
-from typing import Optional
+from enum import IntEnum
+from typing import Dict, Optional, Set
 
 from netCDF4 import Dataset
+import numpy as np
+from numpy import ma
 
 from argortqcpy.profile import ProfileBase
+
+
+class ArgoValueFlag(IntEnum):
+    """Flags for check output."""
+
+    NO_QC = "0"
+    GOOD = "1"
+    PROBABLY_GOOD = "2"
+    PROBABLY_BAD = "3"
+    BAD = "4"
+    CHANGED = "5"
+    # "6" not used
+    # "7" not used
+    ESTIMATED = "8"
+    MISSING = "9"
+    FILL_VALUE = ""
+
+
+FLAG_PRECEDENCE: Dict[ArgoValueFlag, Set] = {
+    ArgoValueFlag.NO_QC: set(),
+    ArgoValueFlag.GOOD: {
+        ArgoValueFlag.NO_QC,
+    },
+    ArgoValueFlag.PROBABLY_GOOD: {
+        ArgoValueFlag.NO_QC,
+        ArgoValueFlag.GOOD,
+        ArgoValueFlag.CHANGED,
+    },
+    ArgoValueFlag.PROBABLY_BAD: {
+        ArgoValueFlag.NO_QC,
+        ArgoValueFlag.GOOD,
+        ArgoValueFlag.PROBABLY_GOOD,
+        ArgoValueFlag.CHANGED,
+    },
+    ArgoValueFlag.BAD: {
+        ArgoValueFlag.NO_QC,
+        ArgoValueFlag.GOOD,
+        ArgoValueFlag.PROBABLY_GOOD,
+        ArgoValueFlag.CHANGED,
+        ArgoValueFlag.PROBABLY_BAD,
+    },
+    ArgoValueFlag.CHANGED: {
+        ArgoValueFlag.NO_QC,
+    },
+    ArgoValueFlag.ESTIMATED: {
+        ArgoValueFlag.NO_QC,
+        ArgoValueFlag.GOOD,
+        ArgoValueFlag.PROBABLY_GOOD,
+    },
+    ArgoValueFlag.MISSING: {
+        ArgoValueFlag.NO_QC,
+    },
+}
+
+
+class CheckOutput:
+    """Class for storing the output of a single check."""
+
+    def __init__(self, profile: ProfileBase) -> None:
+        """Initialise a check output with the profile of interest, and the output data."""
+        self._profile: ProfileBase = profile
+        self._output: Dict[str, ma.MaskedArray] = {}
+
+    def ensure_output_for_property(self, property_name):
+        """Create an output flag array if it does not exist."""
+        if property_name not in self._output:
+            self._output[property_name] = ma.empty_like(self._profile.get_property_data(property_name), dtype="|S1")
+            self._output[property_name][:] = ArgoValueFlag.NO_QC
+
+    def set_output_flag_for_property(
+        self, property_name: str, flag: ArgoValueFlag, where: Optional[np.ndarray] = None
+    ) -> None:
+        """Set a flag for a given property (possibly only on some values) accounting for flag precedence."""
+        self.ensure_output_for_property(property_name)
+        where = where or slice(None)
+        flags = self._output[property_name][where]
+        for overridable_flag in FLAG_PRECEDENCE[flag]:
+            flags[flags == overridable_flag] = flag
 
 
 class CheckBase(ABC):

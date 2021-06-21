c683ca5e1fd47e040a192de898ea5b074e77f47d
Andrew Hale <andhal@bodc.ac.uk>
2021-06-21 11:00:28 +0100

    Fix linting error

--- argortqcpy/checks.py
+++ argortqcpy/checks.py
@@ -173,7 +173,8 @@ class PressureIncreasingCheck(CheckBase):
         )
 
         # do the third pass finding any sections where it has been non-montonic and is still below the last good value
-        running_maximum = np.maximum.accumulate(pressure, axis=-1)  # this is a running maximum, constant parts mean bad values
+        # this is a running maximum, constant parts mean bad values
+        running_maximum = np.maximum.accumulate(pressure, axis=-1)
         running_maximum_constant = np.diff(running_maximum, prepend=-np.inf) == 0.0
         output.set_output_flag_for_properties(
             ["PRES", "TEMP", "PSAL"],
